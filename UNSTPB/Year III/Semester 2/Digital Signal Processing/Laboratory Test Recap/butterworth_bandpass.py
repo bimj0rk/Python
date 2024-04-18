91e2d6eb98bef1ef740f707431fc50b1f8c18a67
Adrian Câmpean <aicampean@gmail.com>
2024-04-18 12:48:13 +0300

    dsp lab test recap

---
+++ UNSTPB/Year III/Semester 2/Digital Signal Processing/Laboratory Test Recap/butterworth_bandpass.py
@@ -0,0 +1,31 @@
+#%%
+
+import numpy as np
+import scipy.signal as sp
+import matplotlib.pyplot as plt
+
+fs = 13000
+ft = 250
+
+fc1 = 4500
+fc2 = 6000
+
+rp = 2
+rs  = 20
+
+wp = np.array([fc1 + ft/2, fc2 - ft/2])/(fs/2)
+ws = np.array([fc1 - ft/2, fc2 + ft/2])/(fs/2)
+
+N, w = sp.buttord(wp, ws, rp, rs)
+b, a = sp.butter(N, w, btype = 'bandpass', output = 'ba')
+w, h = sp.freqz(b, a, worN = 512, plot = None)
+f = (fs/2) * w/np.pi
+
+print(N)
+
+plt.close('all')
+plt.grid()
+plt.plot(f, 20 * np.log10(abs(h)), linewidth = 2)
+plt.show()
+
+# %%
---
+++ UNSTPB/Year III/Semester 2/Digital Signal Processing/Laboratory Test Recap/butterworth_highpass.py
@@ -0,0 +1,28 @@
+#%%
+import numpy as np
+import scipy.signal as sp
+import matplotlib.pyplot as plt
+
+fs = 13000
+ft = 250
+
+fc = 4500
+
+rp = 10
+rs = 20
+
+wp = (fc + ft/2)/(fs/2)
+ws = (fc - ft/2)/(fs/2)
+
+N, w = sp.buttord(wp, ws, rp, rs)
+b, a = sp.butter(N, w, btype = 'high', output = 'ba')
+w, h = sp.freqz(b, a, worN = 512, plot = None)
+f = (fs/2) * w/np.pi
+
+print(N)
+
+plt.close('all')
+plt.grid()
+plt.plot(f, 20 * np.log10(abs(h)), linewidth = 2)
+plt.show()
+# %%
