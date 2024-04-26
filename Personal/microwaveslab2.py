fadadff493d7cb3cc503418ece18768d975d9a6d
Adrian Câmpean <37622282+bimj0rk@users.noreply.github.com>
2024-04-27 00:08:05 +0300

    cleanup

--- Personal/microwaveslab2.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-d = np.array([6, 14, 18, 20, 21, 22, 23, 23.5, 24])
-lambdaG = 26
-
-for i in range(9):
-    print(np.round(np.sqrt(1 + (1/np.sin(np.pi * d[i] / lambdaG))**2), decimals = 2))
