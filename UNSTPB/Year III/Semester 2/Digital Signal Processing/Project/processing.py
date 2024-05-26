de1119def5815094918ca81cb1847bf81d24b75f
Adrian Câmpean <aicampean@gmail.com>
2024-05-27 00:37:22 +0300

    xd

--- UNSTPB/Year III/Semester 2/Digital Signal Processing/Project/processing.py
+++ UNSTPB/Year III/Semester 2/Digital Signal Processing/Project/processing.py
@@ -1,51 +1,50 @@
 import numpy as np
-import scipy.signal as signal
+import scipy.signal as sp
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
 # Load the audio file
-file_path = 'D:\\GitHub Repositories\\Python\\UNSTPB\Year III\\Semester 2\\Digital Signal Processing\\Project\\test.WAV'
+file_path = 'D:\\GitHub Repositories\\Python\\UNSTPB\\Year III\\Semester 2\\Digital Signal Processing\\Project\\0.WAV'
 fs, audio = wavfile.read(file_path)
 
-# Define the notch filter parameters
-f0 = 60.0  # Frequency to be removed (Hz)
-Q = 30.0   # Quality factor
+# Parameters
+fs = 10000
+ft = 250
+fc = 150
+rp = 0.2
+rs = 35
+
+# Frequency calculations
+wp = np.array(fc + ft/2)/(fs/2)
+ws = np.array(fc - ft/2)/(fs/2)
 
 # Design the notch filter
-b, a = signal.iirnotch(f0, Q, fs)
+N, w = sp.buttord(wp, ws, rp, rs)
+b, a = sp.butter(N, w, btype='high')
+
 
 # Apply the notch filter to the audio signal
-filtered_audio = signal.filtfilt(b, a, audio)
+filtered_audio = sp.filtfilt(b, a, audio)
 
-# Save the filtered audio to a new file
-output_path = 'D:\\GitHub Repositories\\Python\\UNSTPB\Year III\\Semester 2\\Digital Signal Processing\\Project\\filtered_audio.wav'
+# Save the normalized filtered audio to a new file
+output_path = 'D:\\GitHub Repositories\\Python\\UNSTPB\\Year III\\Semester 2\\Digital Signal Processing\\Project\\filtered_audio.wav'
 wavfile.write(output_path, fs, filtered_audio.astype(np.int16))
 
-# Plot the frequency response of the notch filter
-freq, h = signal.freqz(b, a, fs=fs)
-plt.figure(figsize=(10, 4))
-plt.plot(freq, 20 * np.log10(abs(h)), 'b')
-plt.title('Notch Filter Frequency Response')
+# Plot the original and filtered audio for comparison
+w, h = sp.freqz(b, a)
+plt.figure(figsize=(12, 6))
+plt.subplot(3, 1, 1)
+plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
+plt.xlim(0, 0.5*fs)
+plt.title("Frequency Response")
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Gain (dB)')
-plt.grid(True)
-plt.show()
-
-# Plot the original and filtered audio signals for comparison
-plt.figure(figsize=(10, 4))
-plt.subplot(2, 1, 1)
+plt.ylabel('Gain')
+plt.grid()
+plt.subplot(3, 1, 2)
 plt.plot(audio)
-plt.title('Original Audio Signal')
-plt.xlabel('Sample Index')
-plt.ylabel('Amplitude')
-plt.grid(True)
-
-plt.subplot(2, 1, 2)
+plt.title('Original Audio')
+plt.subplot(3, 1, 3)
 plt.plot(filtered_audio)
-plt.title('Filtered Audio Signal')
-plt.xlabel('Sample Index')
-plt.ylabel('Amplitude')
-plt.grid(True)
-
+plt.title('Filtered Audio')
 plt.tight_layout()
 plt.show()
