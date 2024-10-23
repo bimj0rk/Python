0a1dcabb189463d773f7d3db638cc02e90302f73
adiicmp <aicampean@gmail.com>
2024-10-23 20:27:39 +0300

    python101 lab 2 task2

--- Personal/Python101/Laborator_2/Task2/task02.py
+++ Personal/Python101/Laborator_2/Task2/task02.py
@@ -7,11 +7,15 @@ def task(*args):
     result = []
 
     ################### TO DO #########################
-    lower = lambda x : 1 if "" == x.lower() else 0
+    lower = lambda x : 1 if x == x.lower() else 0
     not_vowel = lambda x : 1 if x not in "aeiou" else 0
+    not_word = lambda x : 1 if len(x) == 1 else 0
+    not_symbol = lambda x : 1 if x in "abcdefghijklmnopqrstuvwxyz" else 0
+
+    string_verificare = lambda x : 1 if type(x) is str and lower(x) and not_vowel(x) and not_word(x) and not_symbol(x) else 0
 
     for i in args:
-        if(type(i) is int or (type(i) is str and lower(i) and not_vowel(i))):
+        if(type(i) is int or string_verificare(i)):
             result.append(i)
     ###################################################
     return result
--- Personal/Python101/Laborator_2/Task3/task03.py
+++ Personal/Python101/Laborator_2/Task3/task03.py
@@ -6,8 +6,12 @@ def task(register):
     names = []
 
     ################### TO DO #########################
-    
+    check_grade = lambda x : 1 if sum(x.values())/len(x.values()) >= 8.5 else 0
 
+    for i in register:
+        if(check_grade(register.get(i))):
+            print(i)
+            names.append(register.get(i))
     ###################################################
     
     return names
