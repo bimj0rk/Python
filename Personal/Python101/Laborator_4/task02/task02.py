1427733bd749ef9745bed9e73a14e07ca262a375
adiicmp <aicampean@gmail.com>
2024-11-11 16:38:39 +0200

    py101 lab4 partial din nou

--- Personal/Python101/Laborator_4/task02/task02.py
+++ Personal/Python101/Laborator_4/task02/task02.py
@@ -7,6 +7,7 @@ class Image():
         self.columns = columns
         self.max_value = max_value
         self.pixels = pixels
+
     def __str__(self):
         # use this for debugging
         image = ""
@@ -16,23 +17,33 @@ class Image():
                 image += f"{self.pixels[i][j]} "
             image += "\n"
         return image
+    
     def sepia(self):
         # apply sepia filter to the image
+        
+        pixels = []
+        '''
+        for _ in range(self.rows):
+            row = list(map(int, f.readline().strip().split()))
+            row_rgb = np.array(row).reshape(self.columns, 3)
+            pixels.append(row_rgb)
+        '''
         pass
+
     def read(self, filename):
         # read from file and assign the correct parameters (see README and input examples)
         f = open(filename, 'r')
         self.format = f.readline().strip('/n')
-        args = f.readline().strip('/n').split(" ")
-        self.rows = int(args[0])
-        self.columns = int(args[1])
-        self.max_value = f.readline().split('/n')
+        size = f.readline().strip('/n').split(" ")
+        self.rows = int(size[0])
+        self.columns = int(size[1])
+        self.max_value = f.readline().strip('/n')            
 
-        data = f.read().split('/n')
-        r = open('da.txt', 'w')
-        array = np.array(data)
-        r.writelines(array)
-        self.pixels = array.reshape(self.rows, self.columns, 3)
+        for _ in f.read():
+            self.pixels.append(list(map(int, f.readline().strip().split())))
+
+        f.close()
+        
     def write(self, filename):
         # write to file in the correct format (see README and input examples)
         pass
