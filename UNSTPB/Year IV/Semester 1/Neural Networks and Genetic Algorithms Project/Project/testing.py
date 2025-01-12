4b36a19cf3427a2db830bc13c70184bc2119db59
Adrian Câmpean <aicampean@gmail.com>
2025-01-12 15:21:12 +0200

    nn proj somewhat working code

--- UNSTPB/Year IV/Semester 1/Neural Networks and Genetic Algorithms Project/Project/testing.py
+++ UNSTPB/Year IV/Semester 1/Neural Networks and Genetic Algorithms Project/Project/testing.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 import os
 import datetime
 
-labels = pd.read_csv("/type-labels.csv")
+labels = pd.read_csv(r"D:\GitHub Repositories\Python\UNSTPB\Year IV\Semester 1\Neural Networks and Genetic Algorithms Project\Project\type-labels.csv")
 labels.head()
 
 labels["ID"] = labels["ID"].astype(str)
 
-filenames = ["/Train/"+ i + ".jpg" for i in labels["ID"]]
+filenames = [r"D:\GitHub Repositories\Python\UNSTPB\Year IV\Semester 1\Neural Networks and Genetic Algorithms Project\Project\Train\\"+ i + ".jpg" for i in labels["ID"]]
 
-train_image_path = "/Train/"
+train_image_path = r"D:\GitHub Repositories\Python\UNSTPB\Year IV\Semester 1\Neural Networks and Genetic Algorithms Project\Project\Train\\"
 
 if len(os.listdir(train_image_path)) == len(filenames):
   print("Filenames match actual amount of files!!! Proceed.")
@@ -41,11 +41,11 @@ def process_image_mobilenet(image_path, img_size = IMG_SIZE):
   # Read in an image file
   image = tf.io.read_file(image_path)
   # Turn the jpeg image into numerical Tensor with 3 colour channels (Red, Green, Blue)
-  image = tf.image.decode_jpeg(image, channels=3)
+  image = tf.image.decode_jpeg(image, channels = 3)
   # Convert the colour channel values from 0-255 to 0-1 values
   image = tf.image.convert_image_dtype(image, tf.float32)
   # Resize the image to our desired value (224, 224)
-  image = tf.image.resize(image, size=[img_size, img_size])
+  image = tf.image.resize(image, size = [img_size, img_size])
 
   return image
 
@@ -96,6 +96,11 @@ def create_data_batches(x, y = None, batch_size = BATCH_SIZE, valid_data = False
     # Turn the training data into batches
     data_batch = data.batch(batch_size)
     return data_batch
+  
+x = filenames
+y = boolean_labels
+train_data = create_data_batches(x, y)
+train_data.element_spec
 
 # Setup input shape to the model
 INPUT_SHAPE = [None, IMG_SIZE, IMG_SIZE, 3] # batch, height width, colour channels
@@ -108,12 +113,10 @@ MODEL_URL = "https://www.kaggle.com/models/google/mobilenet-v3/TensorFlow2/large
 
  # Create a function which builds a Keras model
 def create_model(input_shape = INPUT_SHAPE, output_shape = OUTPUT_SHAPE, model_url = MODEL_URL):
-    print("Building model with:", model)
-
     # Setup the model layers
     model = tf_keras.Sequential([
        hub.KerasLayer(model_url), # Layer 1 (input layer)
-       tf_keras.layers.Dense(units = output_shape, activation="softmax") # Layer 2 (output layer)
+       tf_keras.layers.Dense(units = output_shape, activation = "softmax") # Layer 2 (output layer)
     ])
 
     # Compile the model
@@ -135,9 +138,8 @@ model.summary()
 # Create a function to build a TensorBoard callback
 def create_tensorboard_callback():
     # Create a log directory for storing TensorBoard logs
-    logdir = os.path.join("/kaggle/working/vehicle-classification-dataset/logs",
+    logdir = os.path.join(r"D:\GitHub Repositories\Python\UNSTPB\Year IV\Semester 1\Neural Networks and Genetic Algorithms Project\Project\logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
                           # Make it so the logs get tracked whenever we run an experiment
-                          datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     return tf_keras.callbacks.TensorBoard(logdir)
 
 model_tensorboard = create_tensorboard_callback()
@@ -145,7 +147,7 @@ model_tensorboard = create_tensorboard_callback()
 # Create early stopping callback
 early_stopping = tf_keras.callbacks.EarlyStopping(monitor = "val_accuracy", patience = 3)
 
-NUM_EPOCHS = 100 #@param {type:"slider", min:10, max:100, step:10}
+NUM_EPOCHS = 10
 
 # Create a function to save a model
 def save_model(model, suffix = None):
@@ -153,9 +155,11 @@ def save_model(model, suffix = None):
     saves a given mode in a models directory and appends a suffix (string).
     """
     # Create a model directory pathname with current time
-    modeldir = os.path.join("/kaggle/working/vehicle-classification-dataset/Models",
+    modeldir = os.path.join(r"D:\GitHub Repositories\Python\UNSTPB\Year IV\Semester 1\Neural Networks and Genetic Algorithms Project\Project\Models",
                             datetime.datetime.now().strftime("%Y%m%d-%H%M%s"))
     model_path = modeldir + "-" + suffix + ".h5" # save format of model
     print(f"Saving model to: {model_path}...")
     model.save(model_path)
     return model_path
+
+model.fit(x = train_data, epochs = NUM_EPOCHS, callbacks = [model_tensorboard, early_stopping])
